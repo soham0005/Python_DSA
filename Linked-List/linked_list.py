96109a1839370e04d5614733f0926d431eefdcc1
Soham <sohamadhyapak0005@gmail.com>
2023-02-27 01:10:21 +0530

    Linked List started

---
+++ Linked-List/linked_list.py
@@ -0,0 +1,9 @@
+class Node:
+    def __init__(self,dataval=None):
+         self.data = dataval
+         self.next = None
+        
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        
