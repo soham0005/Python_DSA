ca87f8a95faef6db04f593fa45ebee5bae425561
Soham <sohamadhyapak0005@gmail.com>
2023-03-24 18:20:02 +0530

    Remove Duplicates from Sorted Array

---
+++ leetcode/18_Remove_Duplicates_fromSortedArray.py
@@ -0,0 +1,9 @@
+def removeDuplicatesFromSoretedArray(nums):
+    i,j=0,1
+    while(i<=j and j<len(nums)):
+        if(nums[i]==nums[j]):
+            j+=1
+        else:
+            nums[i+1] = nums[j]
+            i+=1
+    return i+1
